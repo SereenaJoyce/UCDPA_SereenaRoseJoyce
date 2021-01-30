5f023887ecaaabc3d27eb1e579e4287052b30682
SereenaJoyce <sereenajoyce101@gmail.com>
2021-01-30 15:43:53 +0000

    importing a csv file

--- UCDPA_SereenaRoseJoyce.py
+++ UCDPA_SereenaRoseJoyce.py
@@ -9,3 +9,10 @@ import seaborn as sns
 # API
 api = requests.get('http://api.open-notify.org/astros.json')
 print(api.text)
+
+# importing a csv file
+data=pd.read_csv('netflix_titles.csv', index_col=0)
+print(data.head())
+print(data.shape)
+print(data.columns)
+
